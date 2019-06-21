3cd4610a219bb5191e8c479d612ed086541bad27
HIROK BANIK <hirokbanik@gmail.com>
2019-06-21 12:27:33 +0530

    UX improvements

--- script.js
+++ script.js
@@ -15,56 +15,18 @@ let level = 1;
 let timer = 10;
 let count;
 let oArr = ['+', '-', '*', '/'];
-oArr = ['+', '-'];
 
 btn.addEventListener('click', start);
 
 
 // Functions
-function countDown() {
-    tmr.innerText = timer;
-    count = setInterval(() => {
-        timer--;
-        tmr.innerText = timer;
-        if (timer == 0) {
-            clearInterval(count);
-            submit();
-        }
-    }, 1000);
-}
-
 function start() {
     btn.removeEventListener('click', start);
     btn.addEventListener('click', submit);
     ans.addEventListener('keyup', enter);
+    ans.style.width = '70%';
 
     btn.innerText = 'SUBMIT';
-    load();
-}
-
-function over() {
-    clearInterval(count);
-    btn.innerText = 'PLAY AGAIN'
-    btn.removeEventListener('click', submit);
-    btn.addEventListener('click', start);
-    ans.removeEventListener('click', enter);
-    unload();
-}
-
-function enter(e) {
-    if (e.keyCode == 13) submit();
-}
-
-function submit() {
-    let answer = eval(o1.innerText + o.innerText + o2.innerText);
-    if (btn.innerText == 'SUBMIT') {
-        if (answer == ans.value) {
-            score++;
-        } else {
-            life--;
-        }
-    }
-    console.log(ans.value, answer);
     load();
 }
 
@@ -79,9 +41,12 @@ function load() {
     }
 
     timer = 10;
-    o1.innerText = Math.ceil(Math.random() * 100);
     o.innerText = oArr[Math.floor(Math.random() * level)];
-    o2.innerText = Math.ceil(Math.random() * 100);
+
+    let nums = generateNumber();
+
+    o1.innerText = nums[0];
+    o2.innerText = nums[1];
     ans.value = '';
     ans.disabled = false;
     ans.focus();
@@ -102,6 +67,85 @@ function unload() {
     score = 0;
     level = 1;
     timer = 10;
+}
+
+function countDown() {
+    tmr.innerText = timer;
+    count = setInterval(() => {
+        timer--;
+        tmr.innerText = timer;
+        if (timer == 0) {
+            clearInterval(count);
+            submit();
+        }
+    }, 1000);
+}
+
+function over() {
+    clearInterval(count);
+    btn.innerText = 'PLAY AGAIN'
+    btn.removeEventListener('click', submit);
+    btn.addEventListener('click', start);
+    ans.removeEventListener('click', enter);
+    unload();
+}
+
+function generateNumber() {
+    let operator = o.innerText;
+    if (operator == '+') return plus();
+    if (operator == '-') return minus();
+    if (operator == '*') return multiply();
+    if (operator == '/') return divide();
+}
+
+function minus() {
+    let a = Math.round(Math.random() * 100);
+    let b = Math.round(Math.random() * 100);
+    if (a > b) return [a, b];
+    else return [b, a];
+}
+
+function plus() {
+    let a = Math.round(Math.random() * 100);
+    let b = Math.round(Math.random() * 100);
+    return [a, b];
+}
+
+function multiply() {
+    let a = Math.round(Math.random() * 20);
+    let b = Math.round(Math.random() * 10);
+    return [a, b];
+}
+
+function divide() {
+    let b = Math.ceil(Math.random() * 20);
+    console.log(b);
+    let a = Math.floor(Math.random() * 10);
+    console.log(a);
+    a = a * b;
+    console.log(a);
+    return [a, b];
+}
+
+function levelplus() {
+    if (score % 10 === 0) level = (score / 10) + 1;
+}
+
+function enter(e) {
+    if (e.keyCode == 13) submit();
+}
+
+function submit() {
+    let answer = eval(o1.innerText + o.innerText + o2.innerText);
+    if (btn.innerText == 'SUBMIT') {
+        if (answer == ans.value) {
+            score++;
+            levelplus();
+        } else {
+            life--;
+        }
+    }
+    load();
 }
 
 function select(selector) {
